1bb307cfc9f9ede3084441f07504e95c45106d5c
milesillsley <milesillsley@hotmail.co.uk>
2017-05-08 19:01:40 +0100

    isDivisibleByFive tests

--- spec/fizzBuzzSpec.js
+++ spec/fizzBuzzSpec.js
@@ -1,15 +1,17 @@
 describe('FizzBuzz', function() {
 
   var fizzBuzz;
-  //
-  // beforeEach(function() {
-  //   fizzBuzz = new FizzBuzz();
-  // });
+
+   beforeEach(function() {
+     fizzBuzz = new FizzBuzz();
+   });
 
   it("checks multiples of 3", function() {
-    fizzBuzz = new FizzBuzz();
     expect(fizzBuzz.isDivisibleByThree(3)).toBe(true);
     expect(fizzBuzz.isDivisibleByThree(4)).toBe(false);
   });
-
+  it("checks multiples of 5", function() {
+    expect(fizzBuzz.isDivisibleByFive(5)).toBe(true);
+    expect(fizzBuzz.isDivisibleByFive(7)).toBe(false);
+  });
 });

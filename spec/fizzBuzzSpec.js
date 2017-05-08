8e40f4ef38a537a5e1578b666daf73659d60dd5b
Robert Jones <robert.g.jones@gmail.com>
2017-05-08 19:12:08 +0100

    tests for div by 15

--- spec/fizzBuzzSpec.js
+++ spec/fizzBuzzSpec.js
@@ -10,8 +10,15 @@ describe('FizzBuzz', function() {
     expect(fizzBuzz.isDivisibleByThree(3)).toBe(true);
     expect(fizzBuzz.isDivisibleByThree(4)).toBe(false);
   });
+
   it("checks multiples of 5", function() {
     expect(fizzBuzz.isDivisibleByFive(5)).toBe(true);
     expect(fizzBuzz.isDivisibleByFive(7)).toBe(false);
   });
+
+  it("checks multiples of 3 and 5", function() {
+    expect(fizzBuzz.isDivisibleByfifteen(15)).toBe(true);
+    expect(fizzBuzz.isDivisibleByfifteen(17)).toBe(false);
+  });
+
 });

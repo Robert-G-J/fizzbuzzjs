978e32ddccc125e4b224d6ae015b4647d00c3520
Robert Jones <robert.g.jones@gmail.com>
2017-05-09 11:44:13 +0100

    Starting again for fun

--- spec/fizzBuzzSpec.js
+++ spec/fizzBuzzSpec.js
@@ -1,24 +1,9 @@
-describe('FizzBuzz', function() {
+describe("FizzBuzz", function() {
 
-  var fizzBuzz;
+  var fizzBuzz = new FizzBuzz();
 
-   beforeEach(function() {
-     fizzBuzz = new FizzBuzz();
-   });
-
-  it("checks multiples of 3", function() {
-    expect(fizzBuzz.isDivisibleByThree(3)).toBe(true);
-    expect(fizzBuzz.isDivisibleByThree(4)).toBe(false);
-  });
-
-  it("checks multiples of 5", function() {
-    expect(fizzBuzz.isDivisibleByFive(5)).toBe(true);
-    expect(fizzBuzz.isDivisibleByFive(7)).toBe(false);
-  });
-
-  it("checks multiples of 3 and 5", function() {
-    expect(fizzBuzz.isDivisibleByFifteen(15)).toBe(true);
-    expect(fizzBuzz.isDivisibleByFifteen(17)).toBe(false);
+  it("check divisibilty by 3", function() {
+    expect(fizzBuzz.isDivByThree).toBe(true);
   });
 
 });
